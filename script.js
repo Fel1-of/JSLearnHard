93d8d8ef11bc8a3f3531f708ae217578ef3dd038
Fel1-of <fivehellday@gmail.com>
2021-02-01 22:05:31 +0700

    sec

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 let lang = String(prompt("Выберите и введите язык(en/ru)"));
 
-
-
-//A
-if( lang === "ru"){
-    console.log("Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье.");
-} else{ 
-    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.");
-}
-
-//B
-switch(lang){
-    case "ru":
-        console.log("Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье.");
-        break;
-    case "en":
-        console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.");
-        break;
-}   
-
 //C 
 let ar = [];
 ar['ru'] = ["Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье."];
 ar['en'] = ["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday."];
 
 console.log(ar[lang]);
+
+//A
+if( lang === "ru"){
+    console.log(ar['ru']);
+} else{ 
+    console.log(ar['en']);
+}
+
+//B
+switch(lang){
+    case "ru":
+        console.log(ar['ru']);
+        break;
+    case "en":
+        console.log(ar['en']);
+        break;
+}   
+
+//№2
+
+let namePerson = prompt("Введите имя");
+
+namePerson === "Артём" ? console.log("Директор") : 
+namePerson === "Максим" ? console.log("преподаватель") : console.log("студент");
