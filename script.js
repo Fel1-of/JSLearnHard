a61751a923fce52cfb25301f15299ff06bdd6904
Fel1-of <fivehellday@gmail.com>
2021-02-04 19:43:08 +0700

    fst

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 'use strict';
-//№1
-let arr=prompt("Введите 7 многозначных чисел через ', ':").trim().split(', ');
-
-for(let i=0;i<7;i++){
-    if(arr[i][0]==='2' || arr[i][0]==='4'){
-        console.log(arr[i]);
-    }
+function isNumber(num){
+    return !isNaN(parseFloat(num)) && isFinite(parseFloat(num));
 }
-//№2
-console.log('Простые числа:')
-for(let i=2;i<100;i++){
-    let sum=0;
-    for(let j=1;j<=Math.sqrt(i);j++){
-        if (i%j===0){
-            sum+=1;           
+
+function game(){
+    let attempt = 9;
+    let rand = Math.floor(Math.random() * 100) + 1;
+    let ans = prompt("Угадай число от 1 до 100");
+    function answer(){
+        if(!isNumber(ans) && ans!==null ){
+            ans=prompt("Введи число!");
+        }
+        if(!isNumber(ans) && ans!==null){
+            answer();
+        }
+        return Number(ans);
+    }
+    answer();
+    function MoreLess(){
+        if(ans===null){
+            alert("Игра окончена");
+        } else if(attempt===0){
+            confirm('Попытки закончились, хотите сыграть еще?') ? game() : alert("Игра окончена");
+        }
+        else if(ans>rand){
+            ans=prompt(`Загаданное число меньше. Осталось попыток ${attempt}. Введите новое число:`);
+            attempt--;
+            answer();
+            MoreLess();
+        }
+        else if(ans<rand){
+            ans=prompt(`Загаданное число больше. Осталось попыток ${attempt}. Введите новое число:`);
+            attempt--;
+            answer();
+            MoreLess();
+        } 
+        else{
+            confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') ? game() : alert("Игра окончена");
         }
     }
-    if(sum===1){
-        console.log(i, `Делители этого числа: 1 и ${i}`);
-    }
-    
+    MoreLess();
+
+
+
+
 }
+
+game();
