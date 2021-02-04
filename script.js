14785d3ed4e56e58e846094926967ca4137073c1
Fel1-of <fivehellday@gmail.com>
2021-02-04 22:25:53 +0700

    fnl

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 'use strict';
-function isNumber(num){
+
+function isNumber(num) {
     return !isNaN(parseFloat(num)) && isFinite(parseFloat(num));
 }
 
-function game(){
+function game() {
     let attempt = 9;
     let rand = Math.floor(Math.random() * 100) + 1;
     let ans = prompt("Угадай число от 1 до 100");
-    function answer(){
-        if(!isNumber(ans) && ans!==null ){
-            ans=prompt("Введи число!");
-        }
-        if(!isNumber(ans) && ans!==null){
-            answer();
+
+    function answer() {
+        if (!isNumber(ans) && ans !== null) {
+            ans = prompt("Введи число!");
         }
         return Number(ans);
     }
     answer();
-    function MoreLess(){
-        if(ans===null){
+
+    function moreLess() {
+        if (ans === null) {
             alert("Игра окончена");
-        } else if(attempt===0){
+        } else if (ans > 100 || ans < 1) {
+            ans = prompt("Введите число от 1 до 100!");
+            answer();
+            moreLess();
+        } else if (attempt === 0) {
             confirm('Попытки закончились, хотите сыграть еще?') ? game() : alert("Игра окончена");
-        }
-        else if(ans>rand){
-            ans=prompt(`Загаданное число меньше. Осталось попыток ${attempt}. Введите новое число:`);
+        } else if (ans > rand) {
+            ans = prompt(`Загаданное число меньше. Осталось попыток ${attempt}. Введите новое число:`);
             attempt--;
             answer();
-            MoreLess();
-        }
-        else if(ans<rand){
-            ans=prompt(`Загаданное число больше. Осталось попыток ${attempt}. Введите новое число:`);
+            moreLess();
+        } else if (ans < rand) {
+            ans = prompt(`Загаданное число больше. Осталось попыток ${attempt}. Введите новое число:`);
             attempt--;
             answer();
-            MoreLess();
-        } 
-        else{
+            moreLess();
+        } else {
             confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') ? game() : alert("Игра окончена");
         }
     }
-    MoreLess();
+    moreLess();
 }
 
 game();
