47d19445fe7963edbe60de33dd7a46475daedb13
Fel1-of <fivehellday@gmail.com>
2021-02-05 22:55:46 +0700

    what

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 'use strict';
-let week = [ "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" ];
 let date = new Date();
 
 for (let i in week) {
-    if (week[i] === "Воскресенье" || week[i] === "Суббота") {
+    if (week[i] === "Воскресенье" && +i+1 === +date.getDay()%7 || week[i] === "Суббота" && i+1 === +date.getDay()%7) {
+        document.write(week[i].italics().bold() + "<br \/>");
+    } else if (week[i] === "Воскресенье" || week[i] === "Суббота") {
         document.write(week[i].italics() + "<br \/>");
-    } else if (+i === +date.getDay()) {
+    } else if (i+1 === +date.getDay()%7) {
         document.write(week[i].bold() + "<br \/>");
-    } else{
+    } else {
         document.write(week[i] + "<br \/>");
     }
 }
