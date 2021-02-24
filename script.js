10218305b05457d49d243eb979a7513a2ae8eb2e
Fel1-of <fivehellday@gmail.com>
2021-02-24 21:25:49 +0700

    fst

--- script.js
+++ script.js
@@ -1,51 +1,18 @@
 'use strict';
 
-const start = document.querySelector('.start');
-const reset = document.querySelector('.reset');
-const squad = document.querySelector('.div-animate');
-
-let countAnimate = 0;
-let countAnimateTwo = 0;
-let anim = true;
-
-let interval;
-let animate = function(a) {
-    if (a === '') {
-        countAnimate = 0;
-        countAnimateTwo = 0;
-        anim = true;
-        animate(' ');
-        return;
-    } else if (a !== ' ') {
-        interval = requestAnimationFrame(animate);
-        countAnimate++;
-        countAnimateTwo++;
-    }
-
-    if(countAnimate < 1000) {
-        squad.style.transform = `translateX(${countAnimate}px)
-        rotate(${countAnimate}deg)
-        translateY(${countAnimateTwo * 0.2}px)`;
-    } else if (countAnimate >= 1000 && countAnimate < 2000) {
-        squad.style.transform = `translateX(${2000 - countAnimate}px)
-            rotate(${2000 - countAnimate}deg) translateY(30px)`;
+const input = document.querySelector('input'),
+    text = document.querySelector('p');
+let bull = false,id;
+function write(){
+    text.textContent = input.value;
+    bull = true;
+}
+input.addEventListener('input', ()=>{
+    if(bull){
+        clearTimeout(id);
+        id = setTimeout(write, 3000);
     } else {
-        cancelAnimationFrame(interval);
-    }     
-};
-
-start.addEventListener('click', function(e) {
-    e.preventDefault();
-    if (anim) {
-        interval = requestAnimationFrame(animate);
-        anim = false;
-    } else {
-        anim = true;
-        cancelAnimationFrame(interval);
+        id = setTimeout(write, 3000);
     }
-});
-
-reset.addEventListener('click', function() {
-    cancelAnimationFrame(interval);
-    animate('');
-});
+}
+);
